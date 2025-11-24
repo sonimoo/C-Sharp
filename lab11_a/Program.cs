0330972532094518a8f399cf16193bb8f7d6ab59
Sofia <kalinkovasonea@gmail.com>
2025-11-24 17:41:15 +0200

    add lab11 a

---
+++ lab11_a/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+
+Colors color = Colors.red;
+
+for (int i = 0; i < 6; i++)
+{
+    Actions act = GetAction(color);
+    DoAction(act);
+    color = NextState(color); 
+    Console.WriteLine();
+}
+
+static Actions GetAction(Colors color)
+{
+    switch (color)
+    {
+        case Colors.green:
+            return Actions.go;
+        case Colors.red:
+            return Actions.stop;
+        case Colors.yellow:
+            return Actions.wait;
+        default:
+            Debug.Fail("Неизвестный цвет светофора!");
+            return Actions.stop;
+    }
+}
+
+static void DoAction(Actions act)
+{
+    switch (act)
+    {
+        case Actions.go:
+            Console.WriteLine("Действие - ехать");
+            break;
+        case Actions.stop:
+            Console.WriteLine("Действие - ждать");
+            break;
+        case Actions.wait:
+            Console.WriteLine("Действие - готовиться");
+            break;
+        default:
+            Debug.Fail("Неизвестное действие!");
+            break;
+    }
+}
+
+static Colors NextState(Colors color)
+{
+    color++;
+    if (color > Colors.last)
+        color = Colors.first;
+    return color;
+}
+
+enum Colors
+{
+    red,
+    yellow,
+    green,
+    last = green,
+first = red,
+}
+
+enum Actions
+{
+    go,
+    wait,
+    stop,
+}
